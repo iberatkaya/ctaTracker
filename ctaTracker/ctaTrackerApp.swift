a5995a67dc0de28f4015c96f8b28308c5e305cc0
iberatkaya <beratkaya1998@gmail.com>
2024-04-22 21:18:01 -0500

    Initial Commit

---
+++ ctaTracker/ctaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ctaTrackerApp.swift
+//  ctaTracker
+//
+//  Created by Ibrahim Berat Kaya on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ctaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
